48d2d37d8a9d7cb6a5ce6d68d12fdb2316f5ae55
letrongkhanh <144077765+letrongkhanh@users.noreply.github.com>
2023-12-20 15:09:17 +0700

    Add files via upload

---
+++ src/basic/dev/MainApp4.java
@@ -0,0 +1,30 @@
+package basic.dev;
+
+import java.util.Scanner;
+
+public class MainApp4 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+			Scanner sc = new Scanner(System.in);
+
+			System.out.print("Nhập cạnh a: ");
+			int a = sc.nextInt();
+
+			System.out.print("Nhập cạnh b: ");
+			int b = sc.nextInt();
+
+			System.out.print("Nhập cạnh c: ");
+			int c = sc.nextInt();
+
+			if (a <= 0 || b <= 0 || c <= 0) {
+				System.out.println("Cạnh của tam giác phải là số dương");
+			} else if ((a + b <= c) || (a + c <= b) || (b + c <= a)) {
+				System.out.println("3 cạnh không lập thành tam giác");
+			} else {
+				System.out.println("3 cạnh lập thành tam giác");
+			}
+
+	}
+
+}
